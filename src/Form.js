51c36bf7e1d6bbe08f1c62baa89b0d37f99096c2
geekabhinav007 <abhinav26january@gmail.com>
2024-03-28 20:08:38 +0530

    added responsiveness

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Pokemon from './Pokemon';
+import './Form.css'; // Import the CSS file for the form
 
 function NameForm(){
 
     const [num , setName] = useState('');
     const [submitted , setSubmitted] = useState(false)
+
     const handleChange = (event) => {
         setName(event.target.value);
     };
@@ -12,25 +14,23 @@ function NameForm(){
     const handleSubmit = (event) => {
         event.preventDefault();
         setSubmitted(true);
-       
     };
 
     return (
-        <div>
-        <form onSubmit={handleSubmit}>
-            <label>Pokemon Number : <input type="text" className='input' value={num} onChange={handleChange}/>
-
-            </label>
-            <button type="submit" value = {num} className='counter-button'>Submit</button>
-        </form>
-         {submitted && (
-            <div>
-              <Pokemon num= {num} />
-            </div>
-          )}
+        <div className="form-container"> {/* Apply form-container class here */}
+            <form onSubmit={handleSubmit}>
+                <label>Pokemon Number: 
+                    <input type="text" className='input' value={num} onChange={handleChange}/>
+                </label>
+                <button type="submit" value={num} className='button'>Submit</button>
+            </form>
+            {submitted && (
+                <div>
+                    <Pokemon num={num} />
+                </div>
+            )}
         </div>
-        
-      );
+    );
 }
 
 export default NameForm;
