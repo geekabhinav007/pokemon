bb1d24435d5f4afc11eece5d4886c264665d57c3
geekabhinav007 <abhinav26january@gmail.com>
2024-03-28 20:26:44 +0530

    form fixed

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,12 @@
+// Form.js
+
 import React, { useState } from 'react';
 import Pokemon from './Pokemon';
 import './Form.css'; // Import the CSS file for the form
 
-function NameForm(){
-
-    const [num , setName] = useState('');
-    const [submitted , setSubmitted] = useState(false)
+function NameForm() {
+    const [num, setName] = useState('');
+    const [submitted, setSubmitted] = useState(false);
 
     const handleChange = (event) => {
         setName(event.target.value);
@@ -17,10 +18,17 @@ function NameForm(){
     };
 
     return (
-        <div className="form-container"> {/* Apply form-container class here */}
+        <div className="form-container">
             <form onSubmit={handleSubmit}>
-                <label>Pokemon Number: 
-                    <input type="text" className='input' value={num} onChange={handleChange}/>
+                <label>
+                    Pokemon Number: 
+                    <input 
+                        type="text" 
+                        className='input' 
+                        value={num} 
+                        onChange={handleChange} 
+                        placeholder="Enter Pokemon Number" // Placeholder text
+                    />
                 </label>
                 <button type="submit" value={num} className='button'>Submit</button>
             </form>
