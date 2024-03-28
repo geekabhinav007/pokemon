079db86ccd257604b131b8b85f3edfcc67fa7815
geekabhinav007 <abhinav26january@gmail.com>
2024-03-28 19:48:36 +0530

    added responsiveness

--- src/App.js
+++ src/App.js
@@ -12,15 +12,6 @@ function App() {
         <p className='rule'>Enter a valid number between 1 to 151 or Increment/Decrement  Pokemon to get desire Pokemon Picture. If Pokemon number is not valid it will generate random Pokemon. </p>
         <NameForm />
         <Counter />
-
-        {/* <Greeting name="Abhinav" age="75" />
-        <Greeting name="Aj" age="55" />
-        <h1>This is my first JSX Code and also React code too.</h1>
-        <h2>Hii Kumar Abhinav this side</h2>
-        < HellWorld /> */}
-
-
-
       </header>
     </div>
   );
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,25 +1,29 @@
 import './Pokemon.css';
 
-export default function Pokemon({num}){
-    if(num <= 152 && num > 0){
+export default function Pokemon({num}) {
+    if (num <= 152 && num > 0) {
         let baseUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${num}.png`;
 
         return (
-            <div className='pokemon'>
-                <h1>Pokemon #{num}</h1>
-                <img src={baseUrl} alt="Pokemon"/> 
+            <div className='pokemon-container'> {/* Apply .pokemon-container class here */}
+                <div className='pokemon'>
+                    <h1>Pokemon #{num}</h1>
+                    <img src={baseUrl} alt="Pokemon"/> 
+                </div>
             </div>
         );
-    }else{
-    const baseUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
-    let num = Math.floor(Math.random()*151)+1;
-    let url = baseUrl+String(num)+".png";
-    return (
-        <div className='pokemon'>
-            <h1>Pokemon #{num}</h1>
-            <img src={url} alt="Pokemon"/> 
-        </div>
-    );
-    }
+    } else {
+        const baseUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+        let num = Math.floor(Math.random()*151) + 1;
+        let url = baseUrl + String(num) + ".png";
 
+        return (
+            <div className='pokemon-container'> {/* Apply .pokemon-container class here */}
+                <div className='pokemon'>
+                    <h1>Pokemon #{num}</h1>
+                    <img src={url} alt="Pokemon"/> 
+                </div>
+            </div>
+        );
+    }
 };
